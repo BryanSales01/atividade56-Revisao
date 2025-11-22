31f205491e86982e3dd8c222aabb3181f8df0077
BryanSales01 <paulobryansouzasales5@gmail.com>
2025-11-22 15:38:48 -0300

    atividade56(Revisão)

--- atividade56(Revisão).py
+++ atividade56(Revisão).py
@@ -3,13 +3,18 @@
 # Quantas vezes a letra “a” aparece (maiúscula ou minúscula).
 # Em que posição ocorre a primeira e a última letra “a”.
 
-nota1 = float(input("Digite a primeira nota: "))
-nota2 = float(input("Digite a segunda nota: "))
-media = (nota1 + nota2) / 3
 
-if media >= 7:
-    print("Aprovado")
-elif media == 6:
-    print("Recuperação")
-else:
-    print("Reprovado")
+
+frase = input("Digite uma frase: ")
+semespaco = frase.replace(" ", "")
+quantidadea = frase.count("a")
+quantidadeletras = len(frase)
+posição = frase.find("a")
+posicao_a = frase.rfind("a")
+
+
+print(f"{frase}")
+print(f"Quantidade de letras:{quantidadeletras}")
+print(f"Quantidade de letras (a):{quantidadea}")
+print(f"Primeira letra (a):{posição+1}")
+print(f"Última letra (a):{posicao_a+1}")
